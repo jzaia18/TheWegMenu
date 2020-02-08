91539206b95a942c48c6a9152ca976d5379486c5
Emerald Rafferty <err1482@rit.edu>
2020-02-08 16:15:19 -0500

    Made basic recipe checker protocol for vegan

--- thewegmenu/utils/Alternatives.py
+++ thewegmenu/utils/Alternatives.py
@@ -5,9 +5,41 @@ Takes an ingredient and swaps it for an alternative ingredient based on the user
 
 """
 
+import json
 
-def main() -> None:
-    num = 0
+"""
+checks to see if an ingredient is vegan, if not, it will return a
+vegan alternative
+"""
+def vegan(ingredient:str) -> str:
+    replace = {'milk': 'soymilk',
+               'egg': 'Ener-G egg replacer',
+               'beef': 'tofu',
+               'turkey': 'tofu',
+               'chicken': 'tofu',
+               'pork': 'tofu',
+               'gelatin': 'corn starch',
+               'honey': 'agave nectar',
+               'cheese':'Nutritional yeast flakes',
+               'mayonnaise': 'grape seed oil mayonnaise',
+                'ice cream': 'sherbet'
+                }
+    for i in replace:
+        if i == ingredient:
+            return replace[i]
+
+    return ingredient
+
+
+def main(recipe:list, pref:list) -> None:
+    ingredients = {}
+
+    for i in len(recipe):
+        if 'vegan' in pref:
+            i = vegan(i)
+
+    # with open('ingredients.txt', 'finalRecipe') as outfile:
+    #     json.dump(ingredients, outfile)
 
 
 if __name__ == '__main__':
