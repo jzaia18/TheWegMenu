7d9aa3b7dd5c63ae97f043982bc03765b307d646
Jake Zaia <jlz2369@g.rit.edu>
2020-02-08 12:47:14 -0500

    Initial commit

---
+++ thewegmenu/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from functools import wraps
+import os
+
+app = Flask(__name__)
+app.secret_key = os.urandom(16)
+
+@app.route("/")
+def root():
+    return render_template("base.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host="0.0.0.0")
