effec5723f2cbdea35d5abe5efdd4f86a7964269
Emerald Rafferty <err1482@rit.edu>
2020-02-08 15:08:55 -0500

    Initialized the swap class

---
+++ thewegmenu/utils/Alternatives.py
@@ -0,0 +1,14 @@
+"""
+Author err1482 : Emerald Rafferty
+
+Takes an ingredient and swaps it for an alternative ingredient based on the user dietary preferences
+
+"""
+
+
+def main() -> None:
+    num = 0
+
+
+if __name__ == '__main__':
+    main()
