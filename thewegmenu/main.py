13197356043fd33b9e6469cbf3c3cbef432bb858
Emily Fang <fangemily60@gmail.com>
2020-02-08 18:05:08 -0500

    Home page & routes added

--- thewegmenu/main.py
+++ thewegmenu/main.py
@@ -7,7 +7,19 @@ app.secret_key = os.urandom(16)
 
 @app.route("/")
 def root():
-    return render_template("base.html")
+    return render_template("home.html")
+
+@app.route("/login")
+def root():
+    return render_template("login.html")
+
+@app.route("/signup")
+def root():
+    return render_template("signup.html")
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
