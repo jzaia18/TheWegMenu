df6d5bb507b52598929730c7b393cc611089bb01
Emerald Rafferty <err1482@rit.edu>
2020-02-09 08:22:37 -0500

    minor details

--- thewegmenu/utils/recipe_maker.py
+++ thewegmenu/utils/recipe_maker.py
@@ -7,7 +7,7 @@ from utils.wegmans_utils import get_food_data
 def find_weggie(food: str) -> tuple:
     food = get_food_data(food)
 
-    if not food:
+    if not food[0]:
         return (None, None)
 
     return (food[0]['name'], food[0]['sku'])
@@ -18,7 +18,6 @@ def translate(ingredients: list) -> tuple:
         recipe.append(find_weggie(i))
 
     return recipe
-
 
 if __name__ == '__main__':
     pass
