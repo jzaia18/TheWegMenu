e2c68d47d229c70345fe43d0cd28913095e15a5b
Jake Zaia <jlz2369@g.rit.edu>
2020-02-09 06:43:16 -0500

    Add mongo utils for calendar functions

--- thewegmenu/utils/mongo_utils.py
+++ thewegmenu/utils/mongo_utils.py
@@ -7,6 +7,9 @@ connection = pymongo.MongoClient(dbname)
 db = connection.thewegmenu
 users = db.users
 recipes = db.recipes
+calendars = db.calendars
+
+DAYS = {"SUNDAY": [], "MONDAY": [], "TUESDAY": [], "WEDNESDAY": [], "THURSDAY": [], "FRIDAY": [], "SATURDAY": []}
 
 def get_hashed_password(user, passw):
     return hashlib.sha256((passw + user).encode('utf-8')).hexdigest()
@@ -39,6 +42,35 @@ def get_recipes_by_food(food, results=20):
     random.shuffle(result)
     return result[:results]
 
+def add_recipe_to_calendar(recipe_id, day, username):
+    day = str(day).upper()
+    if day not in DAYS:
+        return False
+    cal = calendars.find_one({"username": username})
+    if cal == None:
+        cal = {"username": username, **DAYS}
+        cal[day].append(recipe_id)
+        calendars.insert_one(cal)
+    else:
+        calendars.update_one(
+            {"username": username},
+            {"$push": {day: recipe_id}},
+        )
+    return True
+
+def remove_recipe_from_calendar(recipe_id, day, username):
+    day = str(day).upper()
+    cal = calendars.find_one({"username": username})
+    if not cal:
+        return False
+    new_arr = cal[day]
+    new_arr.remove(recipe_id)
+    calendars.update_one(
+        {"username": username},
+        {"$set": {day: new_arr}},
+    )
+    return True
+
 
 if __name__ == '__main__':
     # print(register("bob", "1234"))
@@ -46,9 +78,13 @@ if __name__ == '__main__':
     # print(authenticate("bob", "1234"))
     # print(authenticate("bob", "12345"))
     # print(authenticate("bobo", "12345"))
-    if len(sys.argv) < 2:
-        print("Please input a food")
-        exit()
+
+    # if len(sys.argv) < 2:
+    #     print("Please input a food")
+    #     exit()
 
 
-    print(*[x['label']+"\n" for x in get_recipes_by_food(sys.argv[1])])
+    # print(*[x['label']+"\n" for x in get_recipes_by_food(sys.argv[1])])
+
+    #add_recipe_to_calendar(16, "monday", "bob")
+    remove_recipe_from_calendar(12, "sunday", "bob")
